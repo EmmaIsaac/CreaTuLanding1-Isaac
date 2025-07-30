4bc096df9c2d728b499a949953c42e6f85fd98a3
Emmanuel Isaac <160789768+EmmaIsaac@users.noreply.github.com>
2025-07-30 18:34:28 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDJgF_tvgg2ayojy8JDR4U9KpnKV54IxdI",
-    authDomain: "rojoyblanco-db.firebaseapp.com",
-    projectId: "rojoyblanco-db",
-    storageBucket: "rojoyblanco-db.firebasestorage.app",
-    messagingSenderId: "740487088502",
-    appId: "1:740487088502:web:4774bea4bbe87d39a71257",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 };
 
 export const app = initializeApp(firebaseConfig);
